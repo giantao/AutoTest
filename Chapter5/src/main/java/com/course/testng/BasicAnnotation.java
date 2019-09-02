8a6ab4b62d1f8bde89166008d4752be695f569be
giantao <37285879+giantao@users.noreply.github.com>
2019-09-03 00:20:40 +0800

    多线程，超时测试

--- Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
+++ Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -7,12 +7,14 @@ public class BasicAnnotation {
     //最基本的注释，用来把方法标记为测试的一部分
     @Test
     public void testCase1() {
+        System.out.printf("Thread Id : %s%n", Thread.currentThread().getId());
         System.out.println("这是测试用例1");
 
     }
 
     @Test
     public void testCase2(){
+        System.out.printf("Thread Id : %s%n", Thread.currentThread().getId());
         System.out.println("这是测试用例2");
     }
 
